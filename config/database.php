f09431444813060583ca0968ad76f0351ec90d1a
Vladimir Kipriyanov <liar2707@gmail.com>
2026-01-08 11:55:00 +0500

    Настройка ci/деплой render v1.16

--- config/database.php
+++ config/database.php
@@ -85,18 +85,19 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'url' => env('DATABASE_URL'), // Главный источник данных
+            'host' => env('DB_HOST', null),        // null — чтобы не перебивать DATABASE_URL
+            'port' => env('DB_PORT', null),        // null — аналогично
+            'database' => env('DB_DATABASE', null), // null — берём из DATABASE_URL
+            'username' => env('DB_USERNAME', null), // null — берём из DATABASE_URL
+            'password' => env('DB_PASSWORD', null), // null — берём из DATABASE_URL
+            'charset' => env('DB_CHARSET', 'utf8mb4'), // Лучше utf8mb4
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => env('DB_SSLMODE', 'require'), // Обязательно для Render
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
