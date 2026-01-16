6d936529e2e835d0057c5f6f39e425bb72f04c5b
liar74ru <liar13@yandex.ru>
2026-01-17 01:20:11 +0500

    Ответвление пробую отктил миграции 2.25

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -15,8 +15,26 @@
 
         <!-- Кнопки авторизации справа -->
         <div class="nav-auth">
-            <a href="#" class="nav-login disabled">Вход</a>
-            <a href="#" class="nav-register disabled">Регистрация</a>
+            @auth
+                <!-- Для авторизованных -->
+                <span style="margin-right: 15px; color: #666;">
+                {{ Auth::user()->name }}
+            </span>
+                {{--            <a href="{{ route('dashboard') }}" class="nav-button">--}}
+                {{--                Панель--}}
+                {{--            </a>--}}
+                <a href="{{ route('profile.edit') }}" class="auth-button">Профиль</a>
+                <form method="POST" action="{{ route('logout') }}" style="display: inline;">
+                    @csrf
+                    <button type="submit" class="auth-button">
+                        Выйти
+                    </button>
+                </form>
+            @else
+                <!-- Для гостей -->
+                <a href="{{ route('login') }}" class="auth-button">Вход</a>
+                <a href="{{ route('register') }}" class="auth-button">Регистрация</a>
+            @endauth
         </div>
     </div>
 </header>
